326de6a2947807645107deda6f081d7697496dc9
demon5470 <58383755+demon5470@users.noreply.github.com>
2020-03-08 10:11:15 +0200

    Add files via upload
    Problema D. 0.4. Apšilimas
    Įvesties failo vardas: Standartinis įėjimas
    Išvesties failo pavadinimas: Standartinė išvestis
    Terminas: 1 s
    Atminties riba: 1024 MB
    Duota simbolių eilutė. Raskite simbolių eilutės ilgį.
    
    Input
    Viena simbolių eilutė iki 100 simbolių.
    Output
    Išveskite ilgį.
    Example
    Standartinis įėjimas	
    Kalafioras
    Standartinė išvestis
    10

---
+++ solution (12).cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+  char x[101];
+  cin >> x;
+  int ilgis = strlen(x);
+
+  cout << ilgis << endl;
+
+  return 0;
+}
+
+
