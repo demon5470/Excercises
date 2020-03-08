0a58a2ca0257aec47a96bf02c4bfb7b7c1e14788
demon5470 <58383755+demon5470@users.noreply.github.com>
2020-03-08 10:12:33 +0200

    Add files via upload
    Problema B. 0.2. Apšilimas
    Įvesties failo vardas: Standartinis įėjimas
    Išvesties failo pavadinimas: Standartinė išvestis
    Terminas: 1 s
    Atminties riba: 1024 MB
    Duotas vienas simbolis. Patikrinkite, ar tai yra simbolis X.
    
    Input
    Vienas simbolis.
    Output
    Išveskite TAIP arba NE.
    Example
    Standartinis įėjimas	
    X
    Standartinė išvestis
    TAIP

---
+++ solution (14).cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+  char n;
+  int X;
+  cin >> n;
+  if (n == 'X')
+  cout << "TAIP" << endl;
+  else {
+    cout << "NE" << endl;
+  }
+
+  return 0;
+}
+
+
